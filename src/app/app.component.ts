3a7381b75a4b40ff3ff990024ff0df4aa06e8f37
ELVIS COSTA DE OLIVEIRA <elvis.coliveira@senacsp.edu.br>
2023-10-27 20:04:54 -0300

    subida de projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Task } from 'src/models/Task';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  constructor(public dialog: MatDialog) { }
+  //lista de tarefas
+  tarefas: Task[] = [
+    new Task({
+      name: "Estudar Angular",
+      descricao: "Preciso estudar angular..."
+    })
+  ]
+  //metodo para abrir nosso dialog
+  openDialog(): void {
+    const dialogRef = this.dialog.open(MyDialogComponent);
+    dialogRef.afterClosed().subscribe(res =>{
+      if(res){
+        this.tarefas.push(res)
+      }
+    } )
+  }
+  //contador da lista de tarefas
+  get totalDeTarefas(): number{
+    return this.tarefas.length
+  }
+  //total de pendentes
+  get totalDePendentes(): number {
+    return this.tarefas.filter(tarefa => tarefa.situacao === "Pendente").length
+  }
+  //metedo altera status da tarefa
+  alterarSituacao(tarefa: Task, situacao: string): void {
+    tarefa.situacao = situacao
+  }
 }
